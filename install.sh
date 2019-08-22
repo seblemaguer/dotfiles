0e7eec7cd7a736d264bbee651129162f398683b9
Sebastien Le Maguer <lemagues@tcd.ie>
2019-08-22 12:59:10 +0200

    [install] add proper emacs tangling

--- install.sh
+++ install.sh
@@ -4,7 +4,16 @@
 pip install --user dotfiles
 
 # Sync the files
-dotfiles -C dotfilesrc --sync
+dotfiles -C dotfilesrc --sync --force
 
 # Post installation (vim part)
 zsh vim/install.sh
+
+# Compilation of emacs
+emacs -Q --batch --eval "
+    (progn
+      (require 'ob-tangle)
+      (dolist (file command-line-args-left)
+        (with-current-buffer (find-file-noselect file)
+          (org-babel-tangle))))" "~/.emacs.d/main.org"
+
