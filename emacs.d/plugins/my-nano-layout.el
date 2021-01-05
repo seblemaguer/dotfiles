c2cbeede5df4f28b22a486ca5baa66c1521c55af
Sebastien Le Maguer <lemagues@tcd.ie>
2021-01-05 14:48:11 +0000

    [emacs] fix: proper font handling for the surface

--- emacs.d/plugins/my-nano-layout.el
+++ emacs.d/plugins/my-nano-layout.el
@@ -19,7 +19,7 @@
 
 (setq default-frame-alist
       (append (list
-               '(font . "Roboto Mono:style=Light:size=28")
+               '(font . "Roboto Mono:style=Light:size=14")
                ;; '(min-height . 1)  '(height     . 45)
                ;; '(min-width  . 1)  '(width      . 81)
                '(vertical-scroll-bars . nil)
