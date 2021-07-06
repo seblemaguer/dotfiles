c69464a17199d517378e030ce17103f0b2f3607b
Sebastien Le Maguer <lemagues@tcd.ie>
2021-07-06 15:19:41 +0100

    [emacs] clean: python template

--- emacs.d/third_parties/templates/10:.*.py
+++ emacs.d/third_parties/templates/10:.*.py
@@ -12,16 +12,12 @@ LICENSE
     Created: `(format-time-string "%e %B %Y")`
 """
 
-# System/default
-import sys
-import os
+
 
 # Arguments
 import argparse
 
 # Messaging/logging
-import traceback
-import time
 import logging
 from logging.config import dictConfig
 
@@ -33,110 +29,98 @@ LEVEL = [logging.WARNING, logging.INFO, logging.DEBUG]
 ###############################################################################
 # Functions
 ###############################################################################
+def configure_logger(args) -> logger.Logger:
+    """Setup the global logging configurations and instanciate a specific logger for the current script
 
+    Parameters
+    ----------
+    args : dict
+        The arguments given to the script
 
-###############################################################################
-# Main function
-###############################################################################
-def main():
-    """Main entry function
+    Returns
+    --------
+    the logger: logger.Logger
     """
-    global args
+    # create logger and formatter
+    logger = logging.getLogger()
+
+    # Verbose level => logging level
+    log_level = args.verbosity
+    if args.verbosity >= len(LEVEL):
+        log_level = len(LEVEL) - 1
+        # logging.warning("verbosity level is too high, I'm gonna assume you're taking the highest (%d)" % log_level)
+
+    # Define the default logger configuration
+    logging_config = dict(
+        version=1,
+        disable_existing_logger=True,
+        formatters={
+            "f": {
+                "format": "[%(asctime)s] [%(levelname)s] — [%(name)s — %(funcName)s:%(lineno)d] %(message)s",
+                "datefmt": "%d/%b/%Y: %H:%M:%S ",
+            }
+        },
+        handlers={
+            "h": {
+                "class": "logging.StreamHandler",
+                "formatter": "f",
+                "level": LEVEL[log_level],
+            }
+        },
+        root={"handlers": ["h"], "level": LEVEL[log_level]},
+    )
+
+    # Add file handler if file logging required
+    if args.log_file is not None:
+        logging_config["handlers"]["f"] = {
+            "class": "logging.FileHandler",
+            "formatter": "f",
+            "level": LEVEL[log_level],
+            "filename": args.log_file,
+        }
+        logging_config["root"]["handlers"] = ["h", "f"]
+
+    # Setup logging configuration
+    dictConfig(logging_config)
+
+    # Retrieve and return the logger dedicated to the script
+    logger = logging.getLogger(__name__)
+    return logger
+
+
+def define_argument_parser() -> argparse.ArgumentParser:
+    """Defines the argument parser
+
+    Returns
+    --------
+    The argument parser: argparse.ArgumentParser
+    """
+    parser = argparse.ArgumentParser(description="")
+
+    # Add options
+    parser.add_argument("-l", "--log_file", default=None, help="Logger file")
+    parser.add_argument(
+        "-v",
+        "--verbosity",
+        action="count",
+        default=0,
+        help="increase output verbosity",
+    )
+
+    # Add arguments
+    # TODO
+
+    # Return parser
+    return parser
 
 
 ###############################################################################
 #  Envelopping
 ###############################################################################
 if __name__ == "__main__":
-    try:
-        parser = argparse.ArgumentParser(description="")
+    # Initialization
+    arg_parser = define_argument_parser()
+    args = arg_parser.parse_args()
+    logger = configure_logger(args)
 
-        # Add options
-        parser.add_argument("-l", "--log_file", default=None, help="Logger file")
-        parser.add_argument(
-            "-v",
-            "--verbosity",
-            action="count",
-            default=0,
-            help="increase output verbosity",
-        )
-
-        # Add arguments
-        # Example : parser.add_argument("echo", help="description")
-        # TODO
-
-        # Parsing arguments
-        args = parser.parse_args()
-
-        # create logger and formatter
-        logger = logging.getLogger()
-        formatter = logging.Formatter(
-            "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-        )
-
-        # Verbose level => logging level
-        log_level = args.verbosity
-        if args.verbosity >= len(LEVEL):
-            log_level = len(LEVEL) - 1
-            # logging.warning("verbosity level is too high, I'm gonna assume you're taking the highest (%d)" % log_level)
-
-        logging_config = dict(
-            version=1,
-            disable_existing_logger=True,
-            formatters={
-                "f": {
-                    "format": "[%(asctime)s] [%(levelname)s] — [%(name)s — %(funcName)s:%(lineno)d] %(message)s",
-                    "datefmt": "%d/%b/%Y: %H:%M:%S ",
-                }
-            },
-            handlers={
-                "h": {
-                    "class": "logging.StreamHandler",
-                    "formatter": "f",
-                    "level": LEVEL[log_level],
-                }
-            },
-            root={"handlers": ["h"], "level": LEVEL[log_level]},
-        )
-
-        if args.log_file is not None:
-            logging_config["handlers"]["f"] = {
-                "class": "logging.FileHandler",
-                "formatter": "f",
-                "level": LEVEL[log_level],
-                "filename": args.log_file
-            }
-            logging_config["root"]["handlers"] = ["h", "f"]
-
-        dictConfig(logging_config)
-        logger = logging.getLogger(__name__)
-
-        # # create file handler
-        # if args.log_file is not None:
-        #     fh = logging.FileHandler(args.log_file)
-        #     logger.addHandler(fh)
-
-        # Debug time
-        start_time = time.time()
-        logger.info("start time = " + time.asctime())
-
-        # Running main function <=> run application
-        main()
-
-        # Debug time
-        logger.info("end time = " + time.asctime())
-        logger.info(
-            "TOTAL TIME IN MINUTES: %02.2f" % ((time.time() - start_time) / 60.0)
-        )
-
-        # Exit program
-        sys.exit(0)
-    except KeyboardInterrupt as e:  # Ctrl-C
-        raise e
-    except SystemExit:  # sys.exit()
-        pass
-    except Exception as e:
-        logging.error("ERROR, UNEXPECTED EXCEPTION")
-        logging.error(str(e))
-        traceback.print_exc(file=sys.stderr)
-        sys.exit(-1)
+    # TODO: your code comes here
